f7b6f032271d94bce9d7b264c1889233ef240b55
Gary Greyling <greyling.gary@gmail.com>
2009-10-09 16:48:19 +0200

    added buttons and layout

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,8 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def new
+    @student = Student.new
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < ActiveRecord::Base
+  default_scope :order => :first_name
 end
