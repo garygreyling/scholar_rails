557c576be17e3e4ccdefa5a179d5fdfeaae65459
Gary Greyling <greyling.gary@gmail.com>
2009-11-19 19:59:48 +0200

    basic authentication setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :students
-  map.root :controller => 'students'
+  map.resource :user_session
+  map.resources :users
+  map.root :controller => "user_sessions", :action => "new"
 end
 
 # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091006185924) do
+ActiveRecord::Schema.define(:version => 20091119171028) do
 
   create_table "students", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "gender"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",               :null => false
+    t.string   "crypted_password",    :null => false
+    t.string   "password_salt",       :null => false
+    t.string   "persistence_token",   :null => false
+    t.string   "single_access_token", :null => false
+    t.string   "perishable_token",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
