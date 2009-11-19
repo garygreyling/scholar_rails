b16459f1cd92f877d765fd398a473ef8eea82c9f
Gary Greyling <greyling.gary@gmail.com>
2009-11-19 19:07:08 +0200

    create students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,9 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+
+  def create
+    @student = Student.create(params[:student])
+    redirect_to students_path
+  end
 end
